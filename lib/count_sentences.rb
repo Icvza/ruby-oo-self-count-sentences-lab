0d4d88c4b8eaeb7a2ab44940c884925272b3f7df
Isaac Chavez <devisaacalejandro@gmail.com>
2020-11-29 23:16:01 -0500

    commit tree 253528d454b5407c63a6f4a8b953d655aac08150

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.include?(".")
+      true 
+    else 
+      false
+    end
   end
 
-  def question?
 
+  def question?
+    if self.include?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
+    if self.include?("!")
+      true
+    else
+      false
+    end
+
 
   end
 
   def count_sentences
-
+    array = self.split(/[!.?]+/)
+    array.length
   end
 end
